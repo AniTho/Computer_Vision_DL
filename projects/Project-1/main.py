02e40b1617a83530c89f42fb17fe9702c335ce3b
Aniket <aniketthomas27@gmail.com>
2023-01-30 16:05:10 +0530

    Added split percent for valid data and load data
    Added split percent for valid data to ensure that the dataset is split
    into train, valid and test data to avoid any leaking of data and
    overfitting due to hyperparameter tuning. Also added functionality to
    load data from dataloadersAdded split percent for valid data and load
    data
    
    Added split percent for valid data to ensure that the dataset is split
    into train, valid and test data to avoid any leaking of data and
    overfitting due to hyperparameter tuning. Also added functionality to
    load data from dataloaders.

--- projects/Project-1/main.py
+++ projects/Project-1/main.py
@@ -1,10 +1,12 @@
 import split_data
-import os
+from dataload.dataload import load_data
 
 def main():
     images_dir = 'data/UTKFace/images'
-    train_split = 0.8
-    split_data.split(images_dir, train_split)
+    train_split = 0.7
+    valid_split = 0.2
+    split_data.split(images_dir, train_split, valid_split)
+    trainloader, validloader, testloader = load_data()
 
 if __name__ == "__main__":
     main()
